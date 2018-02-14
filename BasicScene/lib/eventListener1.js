2d33b585183974834a4448b8fcebb87bd83d6592
Alberto <a.sanchezu@gmail.com>
2018-02-14 14:31:08 +0100

    Add files via upload
    Añadida carpeta Basic Scene

---
+++ BasicScene/lib/eventListener1.js
@@ -0,0 +1,10 @@
+/**
+ * Created by voiping_ptc11 on 06/02/2018.
+ */
+
+
+var boxE1 = document.querySelector('a-box');
+
+boxE1.addEventListener('mouseenter', function() {
+   boxE1.setAttribute('scale', {x: 2, y: 2, z: 2});
+});
